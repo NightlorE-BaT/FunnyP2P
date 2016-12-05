9e1211ffe1a08e66ad4f04808efad47ffdf421f1
Eugene Shevchenko <eugenesh@pamconsult.com>
2016-12-05 15:25:07 +0300

    update

--- app/src/main/java/by/mythsystems/funnyp2p/MainActivityFragment.java
+++ app/src/main/java/by/mythsystems/funnyp2p/MainActivityFragment.java
@@ -1,12 +1,17 @@
 package by.mythsystems.funnyp2p;
 
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,6 +19,7 @@ import android.widget.TextView;
 public class MainActivityFragment extends Fragment {
 
 	private TextView mWiFiStatusTextView;
+	private ArrayList<ViewGroup> mTableMembers = new ArrayList<ViewGroup>();
 
 	public MainActivityFragment() {
 	}
@@ -21,7 +27,8 @@ public class MainActivityFragment extends Fragment {
 	@Override
 	public View onCreateView( LayoutInflater inflater, ViewGroup container,
 	                          Bundle savedInstanceState ) {
-		return inflater.inflate( R.layout.fragment_main, container, false );
+//		return inflater.inflate( R.layout.fragment_main, container, false );
+		return inflater.inflate( R.layout.fragment_table, container, false );
 	}
 
 	@Override
@@ -34,7 +41,9 @@ public class MainActivityFragment extends Fragment {
 			updateFiFiStatusDisplay();
 		}
 
+		createTable( (ViewGroup)view.findViewById( R.id.viewgroup_table ), 3 );
 		super.onViewCreated( view, savedInstanceState );
+
 	}
 
 	public void updateFiFiStatusDisplay() {
@@ -65,5 +74,17 @@ public class MainActivityFragment extends Fragment {
 		}
 
 		mWiFiStatusTextView.setText( statusId );
+	}
+
+	private void createTable( ViewGroup tableView, int membersCount ) {
+		mTableMembers.clear();
+
+		for ( int mi = 0; mi < membersCount; mi++ ) {
+			ViewGroup memberView = new RelativeLayout( tableView.getContext() );
+			memberView.setBackgroundResource( R.drawable.segment_1_3_white );
+			RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			lParams.addRule( RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE );
+			tableView.addView( memberView, mi, lParams );
+		}
 	}
 }
